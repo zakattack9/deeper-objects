ec685af4b5a7aa74d26c37e2b125fa6756362e5a
zakattack9 <zakattack26111@gmail.com>
2017-03-06 11:00:02 -1000

    research for/in loops

--- app.js
+++ app.js
@@ -4,22 +4,32 @@
 */
 function accessObjectInArray(arr){
 	//Print the parameter being passed through
-
+    //console.log(arr);
 	//How can we tell what type of data this is?
 	//By it's notation, the top level is bracket notation, therefore the parameter is an array
-
+  
 	//What methods can we use for this item?
 	//Array methods
+  	//console.log(arr.length);
 
 	//Access and print the first object in the parameter 
+  	//console.log(arr[0]);
 
 	//Access and print the third object in the parameter
+  	//console.log(arr[2]);
 
 	//Iterate through the array to print all the objects in the parameter
-
+	  for(var i = 0; i < arr.length; i++){
+	  	//console.log("Inner object");
+	  	//console.log(arr[i]);
+	  }
 		//Can you concatanete a string to an object?
+      //No you can not.
 
-		//Iterate through the key:value pairings and print all the keys and their values
+    //Iterate through the key:value pairings and print all the keys and their values
+	    for(var key in arr){
+	    	console.log(arr[key]);
+	    }
 
 	//Clean up your code by creating variable names and setting their values
 }
@@ -39,7 +49,7 @@ var objInArr = [
 	}
 ];
 
-console.log("*** Accessing Objects in Arrays ***\n");
+console.log("*** Accessing Objects in Arrays ***\n"); // "\n" means new line; "\t" represents tab, put in strings
 accessObjectInArray(objInArr);
 console.log("\n*** End ***\n")
 
@@ -48,14 +58,29 @@ console.log("\n*** End ***\n")
 */
 function accessArrayInObject(obj){
 	//Is the parameter an array or an object?
+		//object
 
 	//Print the paramter being passed through the function
+		//console.log(obj);
 
 	//Iterate through the object
+    for(var key in obj){
+    	console.log(key);
+    	console.log(obj[key]);
+    
+    //Create a conditional to check if the key is "cars"
+    	if(key === "cars"){
+    		console.log(obj[key]);
 
-	//Create a conditional to check if the key is "cars"
+      //Iterate through and print the values in the key "cars"
+    		for(var carValues in obj[key]){
+  			  console.log("Values for the key, 'cars'");
+  			  console.log(obj[key][carValues]);
+  		  }
+    	}
+    }
 
-	//Iterate through and print the values in the key "cars"
+	
 
 	//Iterate through and print the key value pairs for each object in the array "toppings"
 
